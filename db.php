b66423e282323580e7ea2b065134076976ff8260
barondevke <ndabasteve1@gmail.com>
2024-09-16 09:37:41 +0300

    db.php

--- db.php
+++ db.php
@@ -1,15 +1,24 @@
  <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ics_e";
+ class Db{
+private $servername = "localhost";
+private $username = "root";
+private $password = "";
+private $dbname = "users";
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+public function connect(){
+
+  $this->conn = null;
+  try {
+      $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $e) {
+      echo "Connection error: " . $e->getMessage();
+  }
+  return $this->conn;
 }
+
+
+
+ }
+
 ?> 
